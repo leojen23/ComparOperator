f57b47dafee64fb0e616123bc83fb76bae14f2e4
VEROFOREST <xvforest@orange.fr>
2020-06-15 15:08:13 +0200

    navbar+modif name page buttonAdmin

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.parallax');
     var instances = M.Parallax.init(elems, null);
   });
-
+// sidenav Js Vf
   document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, null);
