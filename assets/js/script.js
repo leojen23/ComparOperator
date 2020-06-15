8f6e6b79300eff7117e57285ce4052bc6922d8df
VEROFOREST <xvforest@orange.fr>
2020-06-15 18:31:51 +0200

    carousel implamantation
    Signed-off-by: VEROFOREST <xvforest@orange.fr>

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,4 +10,25 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, null);
   });
+// carousel vf
+//   document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.carousel');
+//     var instances = M.Carousel.init(elems,null);
+//   });
 
+//  var instance = M.Carousel.init({
+//     fullWidth: true
+      
+//   });
+  $(document).ready(function(){
+    $('.carousel').carousel();
+
+    setInterval(function(){
+    $('.carousel').carousel('next');
+  
+    },4000);
+
+  });
+
+
+// function for auto play carousel-slider
---
+++ partials/carousel.php
@@ -0,0 +1,10 @@
+
+  
+  <div class="carousel carousel-slider">
+    <a class="carousel-item" href="#one!"><img src="../assets/images/japan1.jpg"></a>
+    <a class="carousel-item" href="#two!"><img src="../assets/images/japan2.jpg"></a>
+    <a class="carousel-item" href="#three!"><img src="../assets/images/japan2.jpg"></a>
+    <a class="carousel-item" href="#four!"><img src="../assets/images/japan2.jpg"></a>
+    <a class="carousel-item" href="#five!"><img src="../assets/images/japan1.jpg"></a>
+  </div>
+      
