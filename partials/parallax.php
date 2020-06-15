f9f84ecc011083c20391af465a5d4185ef44252c
Olivier Guillemot <leojen23@hotmail.com>
2020-06-15 18:26:11 +0200

    parallax set up

---
+++ partials/parallax.php
@@ -0,0 +1,20 @@
+<div class="parallax-container">
+    <div class="parallax"><img src="../assets/images/japan1.jpg"></div>
+</div>
+
+<div class="section white">
+
+    <div class="row container">
+        <h2 class="header">Japon</h2>
+        <p class="grey-text text-darken-3 lighten-3">Découvrez la liste des Tour Opérators proposant des voyages au Japon</p>
+    </div>
+
+    <div class="row">
+      <?php include "operatorCard.php"?>
+    </div>
+</div>
+
+<div class="parallax-container">
+    <div class="parallax"><img src="../assets/images/japan2.jpg"></div>
+</div>
+
