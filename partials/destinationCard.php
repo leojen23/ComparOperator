3699aa7f269dd459bd46729bd7e6a1a812f59a00
VEROFOREST <xvforest@orange.fr>
2020-06-15 18:32:44 +0200

    destinationCards
    Signed-off-by: VEROFOREST <xvforest@orange.fr>

---
+++ partials/destinationCard.php
@@ -0,0 +1,19 @@
+  
+<div class="container">
+  <div class="row">
+         <div class="col s12 ">
+        
+            <?php for ($i=1;$i<=12;$i++):?>
+            
+                <div class="card col s12 m6">
+                    <div class="card-image small">
+                        <img src="../assets/images/japan1.jpg">
+                        <a href="./views/operators.php" class="btn-floating pulse halfway-fab waves-effect waves-light red"><i class="material-icons">flight</i></a>
+                        <span class="card-title">Japan</span>  
+                    </div>
+                </div>
+            <?php endfor;?>
+        </div>
+    
+    </div>
+</div>
