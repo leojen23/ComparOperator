be342207641a08b602397ffb3279f54821082dde
VEROFOREST <xvforest@orange.fr>
2020-06-15 18:33:07 +0200

    navbar->hamburger
    Signed-off-by: VEROFOREST <xvforest@orange.fr>

--- partials/navbar.php
+++ partials/navbar.php
@@ -1,26 +1,27 @@
 <header>
-  <div class="navbar-fixed">
-    <nav class="navbar-fixed">
-      <div class="nav-wrapper ">
-        <a href="#" class="brand-logo center">Logo</a>
-        <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-        <ul id="nav-mobile" class="left hide-on-med-and-down">
-          <li><a href="../views/operators.php">Canada</a></li>
-          <li><a href="../views/operators.php">Congo</a></li>
-          <li><a href="../views/operators.php">Croatie</a></li>
-          <li><a href="../views/operators.php">Espagne</a></li>
-          <li><a href="../views/operators.php">Irlande</a></li>
-        </ul>
-        <ul id="nav-mobile" class="right hide-on-med-and-down">
-          <li><a href="../views/operators.php">Island</a></li>
-          <li><a href="../views/operators.php">Italie</a></li>
-          <li><a href="../views/operators.php">Japon</a></li>
-          <li><a href="../views/operators.php">Madagascar</a></li>
-          <li><a href="../views/operators.php">Russie</a></li>
-        </ul>
-      </div>
-    </nav>
-  </div>
+  
+  <nav class="navbar-fixed">
+    <div class="nav-wrapper ">
+      <a href="../index.php" class="brand-logo center">Logo</a>
+      <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+      <ul id="nav-mobile" class="left hide-on-med-and-down">
+        <li><a href="../views/operators.php">Canada</a></li>
+        <li><a href="../views/operators.php">Congo</a></li>
+        <li><a href="../views/operators.php">Croatie</a></li>
+        <li><a href=
+        "../views/operators.php">Espagne</a></li>
+        <li><a href="../views/operators.php">Irlande</a></li>
+      </ul>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="../views/operators.php">Island</a></li>
+        <li><a href="../views/operators.php">Italie</a></li>
+        <li><a href="../views/operators.php">Japon</a></li>
+        <li><a href="../views/operators.php">Madagascar</a></li>
+        <li><a href="../views/operators.php">Russie</a></li>
+      </ul>
+    </div>
+  </nav>
+   
   <div></div>
   <ul class="sidenav" id="mobile-demo">
     <li><a href="../views/operators.php">Canada</a></li>
@@ -34,4 +35,5 @@
     <li><a href="../views/operators.php">Madagascar</a></li>
     <li><a href="../views/operators.php">Russie</a></li>
   </ul>
+  
 </header>
