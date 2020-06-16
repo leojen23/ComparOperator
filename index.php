fda5eacb7104b7bc248ede0976edcec6d580cf89
VEROFOREST <xvforest@orange.fr>
2020-06-16 09:01:26 +0200

    modif ordre include head
    Signed-off-by: VEROFOREST <xvforest@orange.fr>

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 
-
-<?php include "./partials/carousel.php"?>
 <?php include "./partials/head.php"?>
+<?php include "./partials/carousel.php"?>
 <?php include "./partials/navbar.php"?>
 
 
