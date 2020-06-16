247db953bcc9197b9434ea4718801518952e46b1
Olivier Guillemot <leojen23@hotmail.com>
2020-06-16 14:20:43 +0200

    debeugage

--- partials/parallax.php
+++ partials/parallax.php
@@ -9,13 +9,10 @@
         <p class="grey-text text-darken-3 lighten-3">Découvrez la liste des Tour Opérators proposant des voyages au Japon</p>
     </div>
 
-<<<<<<< HEAD
-    <div class="row center-align">
-      <?php include "operatorCard.php"?>
-=======
+
     <div class="row">
       <?php include "../partials/operatorCard.php"?>
->>>>>>> d0052ebbb1de7dd961e5aa5413cc1375ad391ed3
+
     </div>
 </div>
 
