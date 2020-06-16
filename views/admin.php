3659657660635c962ec95cb5e19353841bd97aca
Olivier Guillemot <leojen23@hotmail.com>
2020-06-16 18:18:13 +0200

    Set up 1st form admin page / (JS not functional, name attribute to be verified and implement, submit button not working

--- views/admin.php
+++ views/admin.php
@@ -2,61 +2,94 @@
 <?php include "../partials/head.php"?>
 <a href="../index.php" class="brand-logo center">Logo</a>
 <body>
+
  <div class="row">
+
+
+ <!-- TAB ------------------------------------------ -->
     <div class="col s12">
       <ul class="tabs">
         <li class="tab col s4"><a href="#test1">Ajouter un Tour Opérateur</a></li>
         <li class="tab col s4"><a  href="#test2">Statut du tour opérateur</a></li>
         <li class="tab col s4"><a href="#test4">Ajouter une Destination</a></li>
-        
       </ul>
     </div>
+
+
+
+
+
     <div id="test1" class="col s12">
         <div class="row">
-        <div class="container">
-        <form class="col s12">
-        <div class="row">
-        <div class="input-field col s6">
-          <input placeholder="Placeholder" id="first_name" type="text" class="validate">
-          <label for="first_name">First Name</label>
-        </div>
-        <div class="input-field col s6">
-          <input id="last_name" type="text" class="validate">
-          <label for="last_name">Last Name</label>
-        </div>
-      </div>
-      <div class="row">
-        <div class="input-field col s12">
-          <input disabled value="I am not editable" id="disabled" type="text" class="validate">
-          <label for="disabled">Disabled</label>
-        </div>
-      </div>
-      <div class="row">
-        <div class="input-field col s12">
-          <input id="password" type="password" class="validate">
-          <label for="password">Password</label>
-        </div>
-      </div>
-      <div class="row">
-        <div class="input-field col s12">
-          <input id="email" type="email" class="validate">
-          <label for="email">Email</label>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col s12">
-          This is an inline input field:
-          <div class="input-field inline">
-            <input id="email_inline" type="email" class="validate">
-            <label for="email_inline">Email</label>
-            <span class="helper-text" data-error="wrong" data-success="right">Helper text</span>
-          </div>
-        </div>
-        </div>
-      </div>
-    </form>
-  </div> 
+          <div class="container">
+            <form class="col s12">
+              <div class="row">
+
+                <div class="input-field col s12">
+                  <input id="name" name="name" type="text" class="validate">
+                  <label for="name">Nom</label>
+                </div>
+                
+                <div class="input-field col s12">
+                  <input id="grade" name="grade" type="text" class="validate">
+                  <label for="Grade">Avis</label>
+                </div>
+
+               
+               <div class = "file-field input-field col s12">
+                  <div class = "btn">
+                     <span>Fichier</span>
+                     <input type ="file" />
+                  </div>
+                  
+                  <div class = "file-path-wrapper"> 
+                     <input class = "file-path validate" type = "text" placeholder="Logo" name="file"/>
+                  </div>
+               </div>
+
+                <!-- Switch -->
+              <div class="input-field col sm12">
+                <div class="checkbox-container">
+                  <label>
+                    <input type="checkbox" name="standard" />
+                    <span>Standard</span>
+                  </label>
+                
+
+                  <label>
+                    <input type="checkbox" name="Premium" />
+                    <span>Premium</span>
+                  </label>
+                </div>
+              </div>
+            </div>
+
+
+              <div class="input-field col s12">
+                  <input id="link" name="link" type="text" class="validate">
+                  <label for="link">site Web</label>
+              </div>
+
+
+              <div>
+                <button class="btn waves-effect waves-light" type="submit" name="action">Submit
+                <i class="material-icons right">send</i>
+              </button>
+              </div>
+            
+            </div>
+              
+              
+            </form>
+         </div> 
+       </div>
     </div>
+
+
+
+
+
+
     <div id="test2" class="col s12">Test 2</div>
     <div id="test3" class="col s12">Test 3</div>
     <div id="test4" class="col s12">Test 4</div>
