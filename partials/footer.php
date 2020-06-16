97a346bd638c34871e67137c87161dcf318b888e
VEROFOREST <xvforest@orange.fr>
2020-06-16 09:04:02 +0200

    modif button admin and footer
    Signed-off-by: VEROFOREST <xvforest@orange.fr>

--- partials/footer.php
+++ partials/footer.php
@@ -1,1 +1,34 @@
+ <footer class="page-footer">
+    <div class="container">
+        <div class="row">
+                <div class="col l6 s12">
+                <h5 class="white-text">Nos principaux partenaires</h5>
+                <ul>
+                    <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                    <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                    <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                    <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                </ul>   
+                <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+                </div>
+                <div class="col l4 offset-l2 s12">
+                    <h5 class="white-text">Contact</h5>
+                    <ul>
+                        <li><a class="grey-text text-lighten-3" href="mailto:xvforest@orange.fr"> <i class="material-icons">email</i>Par mail</a></li>
+                        <li><a class="grey-text text-lighten-3" href="#!"> Rue de mlsjf 42300 ROANNE</a></li>
+                        <li><a class="grey-text text-lighten-3" href="#!">Tel 04.77..98.28.25</a></li>
+                        <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                    </ul>   
+                </div>
+            <div>
+            
+        </div>
+        </div>
+            <div class="footer-copyright">
+            <div class="container">
+            © 2020 Copyright Olivier&Véro
+            <a class="grey-text text-lighten-4 right" href="https://github.com/leojen23/ComparOperator.git">github</a>
+        </div>
+    </div>
+</footer>
 <?php include "./partials/buttonAdmin.php"?>
