a2643be5c4cd0d20d730382874b3fd39c000cf6b
VEROFOREST <xvforest@orange.fr>
2020-06-16 15:57:51 +0200

    photos cards add
    Signed-off-by: VEROFOREST <xvforest@orange.fr>

--- partials/destinationCard.php
+++ partials/destinationCard.php
@@ -5,12 +5,12 @@
             <div class="card col s12 m4 offset-m1">
                 
                 <div class="card-image small">
-                    <img src="../assets/images/japan1.jpg">
+                    <img src="../assets/images/cards/italy.jpg">
                                                 
                     <a href="./views/operators.php" class="btn-floating btn-large pulse halfway-fab waves-effect waves-light red"><i class="material-icons">flight</i></a>
                 
                 </div>
-                <h6 class="card-title card-destination center-align">Japan</h6> 
+                <h6 class="card-title card-destination center-align">Italie</h6> 
             </div>
         <?php endfor;?>
     </div>
