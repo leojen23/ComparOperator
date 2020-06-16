220e578ead8bf3a6deafdbe2a23f187b358ce59c
VEROFOREST <xvforest@orange.fr>
2020-06-16 09:04:54 +0200

    modif destination card
    Signed-off-by: VEROFOREST <xvforest@orange.fr>

--- partials/destinationCard.php
+++ partials/destinationCard.php
@@ -1,19 +1,18 @@
-  
-<div class="container">
-  <div class="row">
-         <div class="col s12 ">
-        
-            <?php for ($i=1;$i<=12;$i++):?>
-            
-                <div class="card col s12 m6">
-                    <div class="card-image small">
-                        <img src="../assets/images/japan1.jpg">
-                        <a href="./views/operators.php" class="btn-floating pulse halfway-fab waves-effect waves-light red"><i class="material-icons">flight</i></a>
-                        <span class="card-title">Japan</span>  
-                    </div>
+
+<div class="row">
+    <div class="container">  
+        <?php for ($i=1;$i<=12;$i++):?>
+            <div class="card col s12 m4 offset-m1">
+                
+                <div class="card-image small">
+                    <img src="../assets/images/japan1.jpg">
+                                                
+                    <a href="./views/operators.php" class="btn-floating btn-large pulse halfway-fab waves-effect waves-light red"><i class="material-icons">flight</i></a>
+                
                 </div>
-            <?php endfor;?>
-        </div>
-    
+                <h6 class="card-title center-align ">Japan</h6> 
+            </div>
+        <?php endfor;?>
     </div>
-</div>
+    
+</div> 
