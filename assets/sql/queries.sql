dafb434ba1f8e0f01077f09c9d1861f197603f61
Olivier Guillemot <leojen23@hotmail.com>
2020-06-17 18:09:37 +0200

    query tests

--- assets/sql/queries.sql
+++ assets/sql/queries.sql
@@ -1,12 +1,24 @@
+
+-- get the location name and image card
 SELECT
     `location`,
     `card_pic`,
     `id_tour_operator`
 FROM
     `destinations`
-WHERE
-    `location` = "Canada"
+GROUP BY `location`
 
 
--- --------------------------------------------------
--- ma dunckflkvdlv
+-- get operator parallax image and destination name
+SELECT
+    `location`,
+    `parallax_1`,
+    `parallax_2`
+FROM
+    `destinations`
+WHERE `location` = "Canada"
+
+
+
+
+
