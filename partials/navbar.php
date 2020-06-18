a156db3cbca9de31e65c6cdece920c487099525c
VEROFOREST <xvforest@orange.fr>
2020-06-18 21:10:13 +0200

    modif navbar et form research
    Signed-off-by: VEROFOREST <xvforest@orange.fr>

--- partials/navbar.php
+++ partials/navbar.php
@@ -1,39 +1,42 @@
 <header>
+  <!-- Dropdown Structure -->
+<ul id="dropdown1" class="dropdown-content">
+  <?php foreach($destinationCards as $card):?>
+  <li><a href="../views/operators.php?destinationName=<?=$card->getLocation()?>"><?=$card->getLocation()?></a></li>
   
+  <?php endforeach;?>
+</ul>
+<div>
   <nav class="navbar-fixed">
     <div class="nav-wrapper ">
       <a href="../index.php" class="brand-logo center">Logo</a>
       <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
       <ul id="nav-mobile" class="left hide-on-med-and-down">
-        <li><a href="../views/operators.php">Canada</a></li>
-        <li><a href="../views/operators.php">Congo</a></li>
-        <li><a href="../views/operators.php">Croatie</a></li>
-        <li><a href="../views/operators.php">Espagne</a></li>
-        <li><a href="../views/operators.php">Irlande</a></li>
+        <!-- Dropdown Trigger -->
+      <li><a class="dropdown-trigger" href="#!" data-target="dropdown1">Sélectionner une destination<i class="material-icons right">arrow_drop_down</i></a></li>
       </ul>
+    
+  
       <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="../views/operators.php" class>Island</a></li>
-        <li><a href="../views/operators.php">Italie</a></li>
-        <li><a href="../views/operators.php">Japon</a></li>
-        <li><a href="../views/operators.php">Madagascar</a></li>
-        <li><a href="../views/operators.php">Russie</a></li>
+      
+        <div class="input-field">
+          <input id="search" type="search" required>
+          <label class="label-icon" for="search"><i class="material-icons">search</i></label>
+          <i class="material-icons">close</i>
+        </div>
+      
       </ul>
     </div>
+    
   </nav>
+  
    
  
   <div>
   <ul class="sidenav" id="mobile-demo">
-    <li><a href="../views/operators.php">Canada</a></li>
-    <li><a href="../views/operators.php">Congo</a></li>
-    <li><a href="../views/operators.php">Croatie</a></li>
-    <li><a href="../views/operators.php">Espagne</a></li>
-    <li><a href="../views/operators.php">Irlande</a></li>
-    <li><a href="../views/operators.php">Island</a></li>
-    <li><a href="../views/operators.php">Italie</a></li>
-    <li><a href="../views/operators.php">Japon</a></li>
-    <li><a href="../views/operators.php">Madagascar</a></li>
-    <li><a href="../views/operators.php">Russie</a></li>
+    <?php foreach($destinationCards as $card):?>
+    <li><a href="../views/operators.php?destinationName=<?=$card->getLocation()?>"><?=$card->getLocation()?></a></li>
+    <?php endforeach;?>
   </ul>
   </div>
   
