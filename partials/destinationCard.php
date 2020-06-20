0af3bb3f4b85218b05384eef40106a425b0b3659
VEROFOREST <xvforest@orange.fr>
2020-06-20 10:30:37 +0200

    scss cards index

--- partials/destinationCard.php
+++ partials/destinationCard.php
@@ -5,12 +5,12 @@
 ?>
 
 <div class="container"> 
-    <div class="row">
+    <div class="row" id="cardDestination">
     
         <?php foreach($destinationCards as $card):
            
             ?>
-            <div class="card col s12 m4 offset-m1">
+            <div class="card col s12 m3" id="smallcard">
                 
                 <div class="card-image small">
                     <img id="imgcard" src="../assets/images/cards/<?=$card->getCard_pic()?>">
