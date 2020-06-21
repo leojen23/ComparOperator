45a60291fca4510ff56b402677e7361a70c02a3e
Olivier Guillemot <leojen23@hotmail.com>
2020-06-21 18:49:12 +0200

    operator card layout

--- partials/operatorCard.php
+++ partials/operatorCard.php
@@ -14,50 +14,49 @@
           $link = "hide";
         break;
       }?>
-    
-    <div class=" container col s12 ">
 
-        <div class="card">
-    
-            <div class="card horizontal z-depth-4 card-operator">
-                
-                <div class="card-image operator-logo">
+<div class="container col s12 ">
+    <div class="card">
+       
+            <div class="operator-card z-depth-2">
+
+                <div class="operator-card-logo ">
                     <img  src="../assets/images/logos/<?= $offer->getLogo()?>">
                 </div>
 
-                <div class="card-stacked">
-                    <div class="card-content">
+                <div class="operator-card-content  ">
 
-                        <div class="card-content-header">
-                            <span class="card-title card-operator"><?= $offer->getName()?></span>
-                            <span class="card-span"><?= $offer->getPrice()?> Euros</span>
-                        </div>
-                        
-                        <div class="card-buttons-container">
-                            <div class="star-grading">
-                                <span><?= $offer->getGrade()?>/5</span>
-                            </div>
+                    <div class="operator-card-content-header">
+                        <h3 class="operator-card-content-header-name"><?= $offer->getName()?></h3>
+                        <div class="operator-card-content-header-price">
+                            <span><?= $offer->getPrice()?>€</span>
                             
-                            <!-- <span class="new badge <?= $btnColor?>" data-badge-caption=<?=$btnText?>></span> -->
-                        </div>
-
-                        <div class="card-content-header">
-                       
-                            <a href="<?= $offer->getLink()?>"><span class="card-span <?= $link?>">  <i class="material-icons">link</i>
-Visiter le site</span></a>
-                        </div>
-
                     </div>
-                </div> 
+                    </div>
 
-                <div class="card-action card-action-container">
-                    <a class="btn-floating btn-large waves-effect waves-light teal modal-trigger" href="#modal2"><i class="small material-icons">comment</i></a>
-                    
+                    <div class="operator-card-content-destination">
+                        <span class="operator-card-content-destination-name"><?=$destinationDisplay->getLocation()?></span>
+                    </div>
+
+                    <div class="operator-card-content-footer">
+                        <span class="operator-card-content-footer-grade teal-text text-darken-2"><?= $offer->getGrade()?><small>/5</small></span>
+                       
+
+                        <a href="<?= $offer->getLink()?>" class="operator-card-content-footer-link waves-effect waves-light btn card-span <?= $link?> "><i class="material-icons left">link</i>SITE OFFICIEL</a>
+                    </div>
                 </div>
-                
-                <?php include "modal.php"?> 
+
+                <div class="operator-card-btn red lighten-5">
+                    <a class="btn-floating btn-large waves-effect waves-light teal modal-trigger" href="#modal2"><i class="small material-icons  ">comment</i></a>
+                </div>
             </div>
-        </div>
+            <?php include "modal.php"?> 
     </div>
+    
+</div>
+    
+    
 
 <?php endforeach;?>
+
+
--- partials/parallax.php
+++ partials/parallax.php
@@ -1,7 +1,7 @@
-<div class="parallax-container valign-wrapper">
+<div class="parallax-container ">
 
     <div>
-        <h1 class="destination-heading"><?= $destinationDisplay->getLocation()?></h1>
+        <h1 class="destination-heading align-center"><?= $destinationDisplay->getLocation()?></h1>
     </div>
 
     <div class="parallax">
@@ -14,8 +14,8 @@
     
 
     <div class="container">
-        <h2 class="header operatortitle center"><?=$destinationDisplay->getLocation()?></h2>
-        <p class="grey-text text-darken-3 lighten-3 center">Découvrez la liste des Tour Opérators proposant des voyages au <?= $destinationDisplay->getLocation()?></p>
+        <h3 class="header center">Liste des Tour-Opérators</h3>
+        <!-- <p class="grey-text text-darken-3 lighten-3 center">Découvrez la liste des Tour Opérators proposant des voyages au <?= $destinationDisplay->getLocation()?></p> -->
     </div>
 
     <div class="container">
@@ -23,6 +23,7 @@
             <?php include "../partials/operatorCard.php"?>
         <!-- </div> -->
     </div>
+</div>
 
 <div class="parallax-container">
     <div class="parallax"><img src="../assets/images/parallax/<?= $destinationDisplay->getParallax_2()?>"></div>
